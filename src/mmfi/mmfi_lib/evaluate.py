fc3705f82ecce8184dd250c0d817b45b52d296b6
Arritmic <arritmic@gmail.com>
2024-10-10 23:39:23 +0300

    Added information about MM-Fi database interface.

--- src/mmfi/mmfi_lib/evaluate.py
+++ src/mmfi/mmfi_lib/evaluate.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-
 def compute_similarity_transform(X, Y, compute_optimal_scale=False):
     """
     A port of MATLAB's `procrustes` function to Numpy.
@@ -25,8 +24,8 @@ def compute_similarity_transform(X, Y, compute_optimal_scale=False):
     X0 = X - muX
     Y0 = Y - muY
 
-    ssX = (X0**2.).sum()
-    ssY = (Y0**2.).sum()
+    ssX = (X0 ** 2.).sum()
+    ssY = (Y0 ** 2.).sum()
 
     # centred Frobenius norm
     normX = np.sqrt(ssX)
@@ -38,28 +37,28 @@ def compute_similarity_transform(X, Y, compute_optimal_scale=False):
 
     # optimum rotation matrix of Y
     A = np.dot(X0.T, Y0)
-    U,s,Vt = np.linalg.svd(A,full_matrices=False)
+    U, s, Vt = np.linalg.svd(A, full_matrices=False)
     V = Vt.T
     T = np.dot(V, U.T)
 
     # Make sure we have a rotation
     detT = np.linalg.det(T)
-    V[:,-1] *= np.sign( detT )
-    s[-1]   *= np.sign( detT )
+    V[:, -1] *= np.sign(detT)
+    s[-1] *= np.sign(detT)
     T = np.dot(V, U.T)
 
     traceTA = s.sum()
 
     if compute_optimal_scale:  # Compute optimum scaling of Y.
         b = traceTA * normX / normY
-        d = 1 - traceTA**2
-        Z = normX*traceTA*np.dot(Y0, T) + muX
+        d = 1 - traceTA ** 2
+        Z = normX * traceTA * np.dot(Y0, T) + muX
     else:  # If no scaling allowed
         b = 1
-        d = 1 + ssY/ssX - 2 * traceTA * normY / normX
-        Z = normY*np.dot(Y0, T) + muX
+        d = 1 + ssY / ssX - 2 * traceTA * normY / normX
+        Z = normY * np.dot(Y0, T) + muX
 
-    c = muX - b*np.dot(muY, T)
+    c = muX - b * np.dot(muY, T)
 
     return d, Z, T, b, c
 
@@ -85,8 +84,3 @@ def calulate_error(preds, gts):
     pampjpe = np.mean(pampjpe)
 
     return mpjpe, pampjpe
-
-
-
-
-
